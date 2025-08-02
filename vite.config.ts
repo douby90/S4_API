c6eb70b7882037ee65806a86b7d1d15cf16fcdfa
douby90 <62137643+douby90@users.noreply.github.com>
2025-08-02 19:05:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/S4_API/', // très important pour GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
